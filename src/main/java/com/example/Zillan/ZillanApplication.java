945479ae5c727997c08a0443669e6827d7cdb6f2
deca <idrisadejumo@gmail.com>
2024-08-02 08:51:24 +0100

    first commit

---
+++ src/main/java/com/example/Zillan/ZillanApplication.java
@@ -0,0 +1,13 @@
+package com.example.Zillan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZillanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZillanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Zillan/ZillanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Zillan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZillanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
